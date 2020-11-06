c6b41fb8e606b9edb7715b96d4dea522889f93ce
Delia Constantino <dconstan92@gmail.com>
2020-11-06 17:58:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,6 +35,7 @@ class Song
 
   def self.find_or_create_by_name(name)
     self.create_by_name(name) unless self.find_by_name(name)
+    
     # if self.find_by_name(name) == nil
     #   self.create_by_name(name)
     # else
